97521f21d705703813c4732c7c9618e810b88d3d
ChoiJiieun <hhhhhhh@naver.com>
2024-09-27 17:28:44 +0900

    no message

---
+++ practice/codetest.py
@@ -0,0 +1,37 @@
+# def solution(my_string, n):
+#     answer = my_string[n:]
+#     return answer
+
+# print(solution('ProgrammerS123', 11))
+# words = 'ProgrammerS123'
+# print(words[10:])
+# num = len(words)-11
+# print(len(words))
+
+parts = [[0, 4], [1, 2], [3, 5], [7, 7]]
+
+my_strings = ["progressive", "hamburger", "hammer", "ahocorasick"]
+
+# print(len(my_strings))
+# print(len(parts))
+
+words = 'hamburger'
+# word = words[parts[0][0]:parts[0][1]]
+# print(words[4])
+# print(word)
+
+answer = ''
+
+# print(type(len(parts)))
+
+# for i in range(len(parts)):
+#     answer += my_strings[i][parts[i][0]:parts[i][1]+1]
+
+# print(answer)
+
+num_list = [2, 1, 6]
+
+# print(num_list[len(num_list)-1])
+
+words = 'abc'
+print('ab' in words)
