233b6c0b24ee2107d67f3b21b2c097693b670b64
Choi <hhhhhhh0115@naver.com>
2024-10-30 04:19:33 +0900

    no message

---
+++ cosmetic_project/test_score.py
@@ -0,0 +1,27 @@
+avg_rating1 = 4.54
+avg_rating2 = 3.57
+avg_rating3 = 4.82
+avg_rating4 = 2.14
+avg_rating5 = 4.95
+avg_rating6 = 3.87
+
+re_count1 = 30
+re_count2 = 29
+re_count3 = 30
+re_count4 = 28
+re_count5 = 24
+re_count6 = 28
+
+rank_score1 = (avg_rating1 * 10) + (re_count1 * 0.5)
+rank_score2 = (avg_rating2 * 10) + (re_count2 * 0.5)
+rank_score3 = (avg_rating3 * 10) + (re_count3 * 0.5)
+rank_score4 = (avg_rating4 * 10) + (re_count4 * 0.5)
+rank_score5 = (avg_rating5 * 10) + (re_count5 * 0.5)
+rank_score6 = (avg_rating6 * 10) + (re_count6 * 0.5)
+
+print(rank_score1)
+print(rank_score2)
+print(rank_score3)
+print(rank_score4)
+print(rank_score5)
+print(rank_score6)
